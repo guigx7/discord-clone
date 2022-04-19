2da1b6bd3b5d03afc6b4deb8b4d59a38b1732f7e
Guilherme Tavares <guigo211103@gmail.com>
2022-04-19 18:00:38 -0300

    Updated

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 import ChannelMessage, { Mention } from "../ChannelMessage";
 import { Container, Messages, InputWrapper, Input, InputIcon, GiftIcon, GifIcon, StickerIcon, EmojiIcon } from "./styles";
 
 const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+  
+  useEffect(() =>{
+    const div = messagesRef.current;
+
+    if(div){
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
   return (
     <Container>
-      <Messages>
+      <Messages ref={messagesRef}>
         <ChannelMessage
           author="guigx7"
           date="Today at 1:53 PM"
